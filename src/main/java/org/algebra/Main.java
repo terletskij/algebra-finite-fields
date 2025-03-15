2ffb827ba4eebb0c6a843fbe56f2839647c0a904
Bogdan <wwwterleckiy@gmail.com>
2025-03-16 00:47:54 +0200

    updated to the current version

--- src/main/java/org/algebra/Main.java
+++ src/main/java/org/algebra/Main.java
@@ -1,7 +1,18 @@
 package org.algebra;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int p = 5;
+
+        System.out.println("Field GF(" + p + "^2):");
+        FiniteFieldGFp2 a = new FiniteFieldGFp2(p, 2, 3);
+        FiniteFieldGFp2 b = new FiniteFieldGFp2(p, 4, 1);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a + b = " + a.add(b));
+        System.out.println("a * b = " + a.multiply(b));
 
     }
 }
